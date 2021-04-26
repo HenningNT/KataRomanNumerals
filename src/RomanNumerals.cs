e99567d06b3070803ada712f8dc7236ea009143b
Henning Torsteinsen <henning.torsteinsen@bouvet.no>
2021-04-26 12:50:02 +0200

    Added solution

---
+++ src/RomanNumerals.cs
@@ -0,0 +1,8 @@
+﻿public class RomanNumerals
+{
+    public string Convert(int input)
+    {
+        return "";
+    }
+}
+}
--- test/RomanNumeralsShould.cs
+++ test/RomanNumeralsShould.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 
 namespace test
 {
-    public class Tests
+    public class RomanNumeralsShould
     {
         [SetUp]
         public void Setup()
@@ -10,9 +10,9 @@ namespace test
         }
 
         [Test]
-        public void Test1()
+        public void ShouldReturnRomanGivenArabic()
         {
-            Assert.Pass();
+            var numero = new RomanNumerals()
+            }
         }
     }
-}
